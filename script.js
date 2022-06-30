d19206935844a83088c40d4a4bcb6088b0a969ff
Adam BEDOUI <adam.bedoui@esprit.tn>
2022-06-30 01:26:35 +0100

    Adapt Code and Tweak Responsive

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 $(document).ready(function () {
   $("#LABEL-NORD, #NORD").mouseover(function () {
+    hideAll();
     $("#card_NORD").addClass("visible");
-  });
-  $("#LABEL-NORD, #NORD").mouseout(function () {
-    $("#card_NORD").removeClass("visible");
   });
 
   $("#LABEL-WEST, #WEST").mouseover(function () {
+    hideAll();
     $("#card_WEST").addClass("visible");
-  });
-  $("#LABEL-WEST, #WEST").mouseout(function () {
-    $("#card_WEST").removeClass("visible");
   });
 
   $("#LABEL-SUD, #SUD").mouseover(function () {
+    hideAll();
     $("#card_SUD").addClass("visible");
-  });
-  $("#LABEL-SUD, #SUD").mouseout(function () {
-    $("#card_SUD").removeClass("visible");
   });
 
   $("#LABEL-OST, #OST").mouseover(function () {
+    hideAll();
     $("#card_OST").addClass("visible");
   });
-  $("#LABEL-OST, #OST").mouseout(function () {
-    $("#card_OST").removeClass("visible");
-  });
 });
+
+function hideAll() {
+  $("#card_NORD, #card_WEST, #card_SUD, #card_OST").removeClass("visible");
+}
